b8512ccd39753236bf6063ed287827c0b4014adc
FazluLKarim4122 <md.fazlulkarim4122@gmail..com>
2024-03-31 21:55:48 +0600

    git authentication added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,5 @@
 
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../Firebase/firebase.init";
 import { useState } from "react";
 
@@ -9,40 +9,58 @@ const Login = () => {
     const auth = getAuth(app)
     // console.log(app)
     const provider = new GoogleAuthProvider()
-    const handleGoogleSignin = ()=>{
+    const handleGoogleSignin = () => {
         // console.log('google sign in clicked')
-        signInWithPopup(auth,provider)
-        .then(result =>{
+        signInWithPopup(auth, provider)
+        .then(result => {
             const loggedInUser = result.user
             console.log(loggedInUser)
             setUser(loggedInUser)
         })
-        .catch(error =>{
+        .catch(error => {
             console.log('error', error.message)
         })
-
+        
     }
-
-    const handleSignOut =() =>{
+    
+    const handleSignOut = () => {
         console.log('sign out')
-        signOut(auth).then(result =>{
+        signOut(auth).then(result => {
             //dekhi kisu paoa jay kina tai result ta dekhay dilam
             console.log(result)
             // thik moto logout hoye gele amra ki korte pari,,at least setUser ke null kore dite pari kono user nai tai
             setUser(null)
-        }).catch(error =>{
+        }).catch(error => {
             console.log(error.message)
         })
     }
+
+    //Github Authentication 
+    const githubProvider = new GithubAuthProvider()
+    const handleGithubSignIn =() =>{
+        console.log('github sign in')
+    }
+    signInWithPopup(auth, githubProvider)
+    .then(result =>{
+        const loggedUser = result.user
+        console.log(loggedUser)
+        setUser(loggedUser)
+    }).catch(error => {
+        console.log(error.message)
+    })
 
     
     return (
         <div>
             {/* user thake ? logout dekhaba : na thakle sign in koro */}
             {
-                user ? 
-                <button onClick={handleSignOut}>Sign Out</button> :
-            <button onClick={handleGoogleSignin}>Google Login</button>}
+                user ?
+                    <button onClick={handleSignOut}>Sign Out</button> :
+                    <div>
+                        <button onClick={handleGoogleSignin}>Google Login</button>
+                        <button onClick={handleGithubSignIn}>GitHub Login</button>
+                    </div>
+            }
             {/** conditional rendering: jodi user thake taile dekhaba na thakle dhukte jabe na */}
             {user && <div>
                 <h3>User: {user?.displayName}</h3>
--- src/__steps.js
+++ src/__steps.js
@@ -1,16 +1,39 @@
 /**
+ * ---------------------------------------------------------
+ *           INITIAL INSTALLATION
+ * -------------------------------------
  * 1. visit: console.firebase.google.com
  * 2. create project (skip google analytics)
  * 3. Register app (create config)
  * 4. install firebase: npm install firebase
  * 5. add config file to your project
  * 6. DANGER : do not publish or make firebase config to public by pushing those to github
+ * -----------------------------------
+ *                 INTEGRATION
+ * ----------------------------
  * 7. go to  docs > build > authentication > web > get started
  * 8. export app from the firebase.config.js file: export default app
  * 9. Login.jsx:  import getAuth from firebase/auth
  * 10. Create const auth = getAuth(app)
+ * ----------------------------------
+ *                    PROVIDER
+ * ------------------------------------------
  * 11. import googleauthprovider and create a new provider
  * 12. use signInWithPopUp and pass auth and provider
  * 13. activate signin method from authorized section(google, github, twitter) to avoid error firebase not installed 
  * 14. [vite]: change 127.0.0.1 to localhost
+ */
+
+/**
+ *               Github firebase application auth provider
+ * ----------------------------------------------------------
+ * at first activate the auth provider (create a)
+ * 1. go to my profile> setting > (scroll down) developer setting
+ * 2. chaile new github application create korte pari
+ * 3. Give a GitHub App name , preview and callback url from firebase sign in method
+ * 4. webhook apatoto not active dilam, jodi kono hook use kori tahole active kore dibo 
+ * 5. then check Only on this account > create github app
+ * 6. then client ID and client secrets(generate) set in firebase 
+ * 7. then to add go to web > github
+ * 
  */
